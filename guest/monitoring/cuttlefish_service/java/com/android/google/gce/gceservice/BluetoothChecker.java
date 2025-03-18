002cd4b7c67113815554e4ba8b36fe4512f98fd1
Tommy Webb <tommy@calyxinstitute.org>
2025-03-18 18:12:50 +0000

    Remove Bluetooth boot dependency
    CalyxOS ships with Bluetooth off by default, so do not treat the boot
    as failed if it doesn't appear.
    
    Issue: calyxos#3082
    Change-Id: I951d599154bf6076a2d0403ebf7f00cd122e76f8

--- guest/monitoring/cuttlefish_service/java/com/android/google/gce/gceservice/BluetoothChecker.java
+++ guest/monitoring/cuttlefish_service/java/com/android/google/gce/gceservice/BluetoothChecker.java
@@ -41,7 +41,7 @@ public class BluetoothChecker extends JobBase {
     public BluetoothChecker(Context context) {
         super(LOG_TAG);
         PackageManager pm = context.getPackageManager();
-        boolean hasBluetooth = pm.hasSystemFeature(PackageManager.FEATURE_BLUETOOTH);
+        boolean hasBluetooth = false;
         if (!hasBluetooth) {
             Log.i(LOG_TAG, "Bluetooth checker disabled (feature missing)");
             mEnabled.set(false);
